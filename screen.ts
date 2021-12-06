41a34406818a710413aa04993de4d02eb598afd0
pai <69026987+paij0se@users.noreply.github.com>
2021-12-06 01:41:10 -0500

    first commit

---
+++ screen.ts
@@ -0,0 +1,30 @@
+export function screenRecord( 
+  video_size: string,
+  video_fps: number,
+  video_time: string,
+  video_name: string
+): void {
+  switch (process.platform) {
+    case "linux" || "darwin":
+      const command: string = `ffmpeg -video_size ${video_size} -framerate ${video_fps} -f x11grab -i :1.0 -c:v libx264 -qp 0 -preset ultrafast -t ${video_time} ${video_name}`;
+      const exec = require("child_process").exec;
+      exec(command, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+    case "win32":
+      const commandWindows: string = `ffmpeg -video_size ${video_size} -framerate ${video_fps} -f x11grab -i :0.0 -c:v libx264 -qp 0 -preset ultrafast -t ${video_time} ${video_name}`;
+      exec(commandWindows, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`exec error: ${error}`);
+          return;
+        }
+        console.log(`stdout: ${stdout}`);
+        console.log(`stderr: ${stderr}`);
+      });
+  }
+}
---
+++ test.ts
@@ -0,0 +1,3 @@
+import { screenRecord } from "./screen";
+//            resolution | fps | duration | file name
+screenRecord("1920x1080", 30, "00:00:15", Math.random() + ".mp4");
